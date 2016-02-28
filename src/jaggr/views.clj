181022788f9c5e24312a3946b6acba3b2a98134a
Steffen Frank <sfrank.home@gmail.com>
2016-02-28 20:02:53 +0100

    the full screen is either red yellow or green

--- src/jaggr/views.clj
+++ src/jaggr/views.clj
@@ -2,6 +2,24 @@
   (:use [hiccup core page]
         [jaggr.jenkins]))
 
+(defn job-details [job]
+  (when-not (empty? job)
+    (html5
+      [:div.job
+       [:div.job-name (h (:name job))]
+       (when-not (empty? (:reason job))
+         [:div.job-reason "'" (h (:reason job)) "'"])
+       (when-not (empty? (:claimedBy job))
+           [:div.job-claimed-by "was heroically claimed by" (h (:claimedBy job))])
+       ])))
+
+(defn job-list [jobs]
+  (when-not (empty? jobs)
+    (html5
+      [:div.job-list
+       (for [job jobs] (job-details job))])))
+
+
 (defn index-page []
   (let [failed-jobs (get-failed-jobs)]
     (html5
@@ -10,12 +28,28 @@
        [:title "JAGGR"]
        (include-css "/css/style.css")]
       [:body
-       [:div {:class "red failed"}
-        [:h1 "Unclaimed failed jobs!"]
-        [:div (str (:unclaimed failed-jobs))]]
-       [:div {:class "yellow failed"}
-        [:h1 "All failed jobs are claimed"]
-        [:div (str (:claimed failed-jobs))]]
-       [:div {:class "grey failed"}
-        [:h1 "Some minor jobs failed"]
-        [:div (str (:unclaimable failed-jobs))]]])))
+       (cond
+         (not-empty (:unclaimed failed-jobs))
+         [:div.red.fullscreen
+          [:h1 "BROKEN BUILDS! CLAIM AND FIX!"]
+          [:div.subtext
+           "have a look at the broken builds - find someone who can fix the problem - claim the builds and fix them"]
+          (job-list (:unclaimed failed-jobs))]
+
+         (not-empty (:claimed failed-jobs))
+         [:div.yellow.fullscreen
+          [:h1 "BROKEN BUILDS - HELP IS ON THE WAY"]
+          [:div.subtext
+           "see if you can help - check in with care - be careful with merges"]
+          (job-list (:claimed failed-jobs))]
+
+         (not-empty (:unclaimable failed-jobs))
+         [:div.green.fullscreen
+          [:h1 "MOST BUILDS MOSTLY OK"]
+          [:div.subtext "have a look at some of these jobs as well"]
+          (job-list (:unclaimable failed-jobs))]
+
+         :else
+         [:div.green.fullscreen
+          [:h1 "HOORAY!"]
+          [:div.subtext "all builds are green!"]])])))
