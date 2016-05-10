ef457bf2065cfadff13ed435f8fd6d69c9ced084
Steffen Frank <sfrank.home@gmail.com>
2016-05-10 06:49:48 +0200

    use html img for background (instead of css background url)

--- src/jaggr/views.clj
+++ src/jaggr/views.clj
@@ -1,4 +1,3 @@
-
 (ns jaggr.views
   (:use [hiccup core page]
         [jaggr.jenkins])
@@ -8,6 +7,13 @@
 
 
 
+(defn header []
+  [:head
+   [:meta {:http-equiv "refresh" :content (str (config/get :refresh-rate))}]
+   [:title "JAGGR"]
+   (include-css "/css/style.css")
+   (include-css "https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic")
+   [:link {:rel "shortcut icon" :href "img/favicon.ico"}]])
 
 (defn job-details [job]
   (when job
@@ -25,15 +31,6 @@
      (for [job jobs] (job-details job))]))
 
 
-(defn header []
-  [:head
-   [:meta {:http-equiv "refresh" :content (str (config/get :refresh-rate))}]
-   [:title "JAGGR"]
-   (include-css "/css/style.css")
-   (include-css "https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic")
-   [:link {:rel "shortcut icon" :href "img/favicon.ico"}]])
-
-
 (defn index-page []
   (try
     (let [failed-jobs (get-failed-jobs)]
@@ -43,24 +40,30 @@
          [:img#logo {:src "/img/jaggr-logo.png"}]
          (cond
            (not-empty (:unclaimed failed-jobs))
-           [:div.red.fullscreen
-            [:h1 "BROKEN BUILDS - CLAIM AND FIX THEM!"]
-            [:div.subtext
-             "have a look at the broken builds - find someone who can fix the problem - claim the builds and fix them"]
-            (job-list (:unclaimed failed-jobs))]
+           [:div.fullscreen
+            [:img {:src "/backgroundimage"}]
+            [:div.fullscreen.red
+             [:h1 "BROKEN BUILDS - CLAIM AND FIX THEM!"]
+             [:div.subtext
+              "have a look at the broken builds - find someone who can fix the problem - claim the builds and fix them"]
+             (job-list (:unclaimed failed-jobs))]]
 
            (not-empty (:claimed failed-jobs))
-           [:div.yellow.fullscreen
-            [:h1 "BROKEN BUILDS - HELP IS ON THE WAY!"]
-            [:div.subtext
-             "see if you can help - check in with care - be careful with merges"]
-            (job-list (:claimed failed-jobs))]
+           [:div.fullscreen
+            [:img {:src "/backgroundimage"}]
+            [:div.yellow.fullscreen
+             [:h1 "BROKEN BUILDS - HELP IS ON THE WAY!"]
+             [:div.subtext
+              "see if you can help - check in with care - be careful with merges"]
+             (job-list (:claimed failed-jobs))]]
 
            (not-empty (:unclaimable failed-jobs))
-           [:div.green.fullscreen
-            [:h1 "MOST BUILDS ARE MOSTLY OK"]
-            [:div.subtext "maybe have a look at some of these jobs as well"]
-            (job-list (:unclaimable failed-jobs))]
+           [:div.fullscreen
+            [:img {:src "/backgroundimage"}]
+            [:div.green.fullscreen
+             [:h1 "MOST BUILDS ARE MOSTLY OK"]
+             [:div.subtext "maybe have a look at some of these jobs as well"]
+             (job-list (:unclaimable failed-jobs))]]
 
            :else
            [:div.green.fullscreen
@@ -72,36 +75,39 @@
       (html5
         (header)
         [:body
-         [:div.fullscreen.error
-          [:h1 "SOMETHING IS WRONG HERE"]
-          [:div.subtext "is Jenkins accessible? - wrong parameters? - network problems?"]
-          [:div "I tried to access " (config/get :base-url) " with user " (config/get :user)]
-          [:div "trying again every " (config/get :refresh-rate) " seconds"]]]))))
+         [:img#logo {:src "/img/jaggr-logo.png"}]
+         [:div.fullscreen
+          [:img {:src "/img/jaggr-logo.png"}]
+          [:div.fullscreen.error
+           [:h1 "SOMETHING IS WRONG HERE"]
+           [:div.subtext "is Jenkins accessible? - wrong parameters? - network problems?"]
+           [:div "I tried to access " (config/get :base-url) " with user " (config/get :user)]
+           [:div "trying again every " (config/get :refresh-rate) " seconds"]]]]))))
 
 
 (defn selectRandomImageFrom [directory]
   (def files (.listFiles (io/file directory)))
-    :else
-      { 
-        :status 200
-        :body (io/file (rand-nth files))
-      })
-
-(defn redirectToImageService[]
+  :else
   {
-    :status 302
-    :headers { "Location" "http://lorempixel.com/g/400/200" }
-    :body ""
-  })
+   :status 200
+   :body   (io/file (rand-nth files))
+   })
+
+(defn redirectToImageService []
+  {
+   :status  302
+   :headers {"Location" "http://lorempixel.com/g/400/200"}
+   :body    ""
+   })
 
 
 (defn imageFrom [directory]
   (def files (.listFiles (io/file directory)))
   (cond
     (empty? files)
-      (redirectToImageService)
+    (redirectToImageService)
     :else
-      (selectRandomImageFrom directory)))
+    (selectRandomImageFrom directory)))
 
 
 (defn green-page []
@@ -120,8 +126,8 @@
   (let [failed-jobs (get-failed-jobs)]
     (cond
       (not-empty (:unclaimed failed-jobs))
-        (red-page)
+      (red-page)
       (not-empty (:claimed failed-jobs))
-        (yellow-page)
+      (yellow-page)
       :else
-        (green-page))))
+      (green-page))))
